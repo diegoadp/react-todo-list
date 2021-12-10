656ac161feac6e71bf173d819967a8b190c32aec
diegoadp <83314316+diegoadp@users.noreply.github.com>
2021-12-10 18:09:00 -0300

    feat: create FilterButton component

---
+++ src/components/FilterButton/FilterButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FilterButton() {
+  return (
+    <button type="button" aria-pressed="true">
+      <span>Show </span>
+      <span>All </span>
+      <span>Tasks</span>
+    </button>
+  );
+}
+
+export default FilterButton;
